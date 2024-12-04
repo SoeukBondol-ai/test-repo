116fde599f1e113278144b6e21d7ec4963dca022
Seouk Bondol-ai <e20221592@dtc1.itc.edu.kh>
2024-12-04 10:34:55 +0700

    addding more code

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ user = input("What is your name?: ")
 print("Hello", user)
 age = input(int("How old are you?: "))
 print(f"You are {age} years old")
+
+salary = input(float("What is your salary?: "))
+print(f"Your salary is {salary}")
